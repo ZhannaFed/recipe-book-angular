96c6b2697dad1592efd1ca89f68492102ca7df2a
Zhanna Fedulova <nesterova.zhannet@gmail.com>
2024-02-09 16:45:57 +0500

    added recipe edit form and shopping list form functionallity

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -25,4 +25,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.igChangedSub.unsubscribe();
   }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
 }
